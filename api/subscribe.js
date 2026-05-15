685cb90bb0ced4662c1bbf31876d86d59aba70da
scheter99 <peter.schiretz@gmail.com>
2026-05-15 11:04:57 +0200

    add more exact error messages

--- api/subscribe.js
+++ api/subscribe.js
@@ -55,7 +55,7 @@ export default async function handler(req, res) {
       console.error("Brevo Error:", data);
 
       return res.status(400).json({
-        error: "Failed to subscribe"
+        error: data?.message || data?.code || "Failed to subscribe"
       });
 
     }
